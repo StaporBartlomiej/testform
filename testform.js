1cfe40d4f0257bbce41521a061132dbc0759eea8
bstapor <bartlomiej.stapor@gmail.com>
2024-02-04 16:04:59 +0100

    XD

---
+++ testform.js
@@ -0,0 +1,21 @@
+function noFunction() {
+    const button = document.getElementById('No');
+    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+}
+
+function yesFunction() {
+    const centeredDiv = document.getElementById('centered');
+    centeredDiv.style.visibility = "hidden";
+    centeredDiv.style.display = "none";
+    const centeredDiv2 = document.getElementById('centered2');
+    centeredDiv2.style.visibility = "visible";
+    centeredDiv2.style.display = "block";
+    const waitVideo = document.getElementById('myVideo');
+    waitVideo.pause();
+    waitVideo.currentTime = 0;
+    const yesVideo = document.getElementById('yesVideo');
+    yesVideo.load();
+    yesVideo.muted = false;
+    yesVideo.play();
+}
